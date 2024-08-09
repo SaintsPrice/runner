ed6f390a63f9e63ca94e9bea720d901cf1614b94
Maxim <kerunov.m@firstmk.ru>
2024-08-09 19:32:34 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {StartPageComponent} from "./components/start-page/start-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StartPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'kotenocka';
 }
